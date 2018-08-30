91ca10b11543a7088c696308543c09f683f6e28d
Valdas Rakutis <36191877+UndeadRat22@users.noreply.github.com>
2018-08-30 14:58:54 +0300

    Add files via upload

---
+++ flysas_crawler.py
@@ -0,0 +1,98 @@
+flysas_url = "https://classic.flysas.com/en/us/"
+
+payload = {
+    #"__EVENTTARGET" : "ctl00$FullRegion$MainRegion$ContentRegion$ContentFullRegion$ContentLeftRegion$CEPGroup1$CEPActive$cepNDPRevBookingArea$Searchbtn$ButtonLink",
+    #"__EVENTARGUMENT" : "",
+    "ctl00$FullRegion$TopRegion$_siteHeader$hdnProfilingConsent" : "",
+    "ctl00$FullRegion$TopRegion$_siteHeader$hdnTermsConsent" : "",
+    "ctl00$FullRegion$TopRegion$_siteHeader$_ssoLogin$MainFormBorderPanel$uid" : "",
+    "ctl00$FullRegion$TopRegion$_siteHeader$_ssoLogin$MainFormBorderPanel$pwd" : "",
+    "ctl00$FullRegion$TopRegion$_siteHeader$_ssoLogin$MainFormBorderPanel$hdnShowModal" : "",
+    "ctl00$FullRegion$TopRegion$_siteHeader$_ssoLogin$MainFormBorderPanel$hdnIsEb0" : "",
+    #roundtrip
+    "ctl00$FullRegion$MainRegion$ContentRegion$ContentFullRegion$ContentLeftRegion$CEPGroup1$CEPActive$cepNDPRevBookingArea$ceptravelTypeSelector$TripTypeSelector" : "roundtrip",
+    "ctl00$FullRegion$MainRegion$ContentRegion$ContentFullRegion$ContentLeftRegion$CEPGroup1$CEPActive$cepNDPRevBookingArea$predictiveSearch$hiddenIntercont" : "False",
+    "ctl00$FullRegion$MainRegion$ContentRegion$ContentFullRegion$ContentLeftRegion$CEPGroup1$CEPActive$cepNDPRevBookingArea$predictiveSearch$hiddenDomestic" :"SE,GB",
+    "ctl00$FullRegion$MainRegion$ContentRegion$ContentFullRegion$ContentLeftRegion$CEPGroup1$CEPActive$cepNDPRevBookingArea$predictiveSearch$hiddenFareType" :"",
+    #from
+    "ctl00$FullRegion$MainRegion$ContentRegion$ContentFullRegion$ContentLeftRegion$CEPGroup1$CEPActive$cepNDPRevBookingArea$predictiveSearch$txtFrom" :"ARN",
+    #from
+    "ctl00$FullRegion$MainRegion$ContentRegion$ContentFullRegion$ContentLeftRegion$CEPGroup1$CEPActive$cepNDPRevBookingArea$predictiveSearch$hiddenFrom" :"ARN",
+    #to
+    "ctl00$FullRegion$MainRegion$ContentRegion$ContentFullRegion$ContentLeftRegion$CEPGroup1$CEPActive$cepNDPRevBookingArea$predictiveSearch$txtTo" :"LHR",
+    #to
+    "ctl00$FullRegion$MainRegion$ContentRegion$ContentFullRegion$ContentLeftRegion$CEPGroup1$CEPActive$cepNDPRevBookingArea$predictiveSearch$hiddenTo" :"LHR",
+    "ctl00$FullRegion$MainRegion$ContentRegion$ContentFullRegion$ContentLeftRegion$CEPGroup1$CEPActive$cepNDPRevBookingArea$predictiveSearch$txtFromTOJ" :"Type a country, city or airport",
+    "ctl00$FullRegion$MainRegion$ContentRegion$ContentFullRegion$ContentLeftRegion$CEPGroup1$CEPActive$cepNDPRevBookingArea$predictiveSearch$hiddenFromTOJ" :"",
+    #outbound
+    "ctl00$FullRegion$MainRegion$ContentRegion$ContentFullRegion$ContentLeftRegion$CEPGroup1$CEPActive$cepNDPRevBookingArea$cepCalendar$hiddenOutbound" :"2018-10-08",
+    #return
+    "ctl00$FullRegion$MainRegion$ContentRegion$ContentFullRegion$ContentLeftRegion$CEPGroup1$CEPActive$cepNDPRevBookingArea$cepCalendar$hiddenReturn" :"2018-10-14",
+    "ctl00$FullRegion$MainRegion$ContentRegion$ContentFullRegion$ContentLeftRegion$CEPGroup1$CEPActive$cepNDPRevBookingArea$cepCalendar$hdnSelectedOutboundMonth" :"",
+    "ctl00$FullRegion$MainRegion$ContentRegion$ContentFullRegion$ContentLeftRegion$CEPGroup1$CEPActive$cepNDPRevBookingArea$cepCalendar$hdnSelectedReturnMonth" :"",
+    "ctl00$FullRegion$MainRegion$ContentRegion$ContentFullRegion$ContentLeftRegion$CEPGroup1$CEPActive$cepNDPRevBookingArea$cepCalendar$hiddenReturnCalVisible" :"",
+    "ctl00$FullRegion$MainRegion$ContentRegion$ContentFullRegion$ContentLeftRegion$CEPGroup1$CEPActive$cepNDPRevBookingArea$cepCalendar$hiddenStoreCalDates" :"Thu Aug 30 2018 00:00:00 GMT+0300 (Eastern European Summer Time),Thu Aug 30 2018 00:00:00 GMT+0300 (Eastern European Summer Time),Sat Aug 24 2019 00:00:00 GMT+0300 (Eastern European Summer Time)",
+    #outound
+    "ctl00$FullRegion$MainRegion$ContentRegion$ContentFullRegion$ContentLeftRegion$CEPGroup1$CEPActive$cepNDPRevBookingArea$cepCalendar$selectOutbound" :"2018-08-01",
+    #return
+    "ctl00$FullRegion$MainRegion$ContentRegion$ContentFullRegion$ContentLeftRegion$CEPGroup1$CEPActive$cepNDPRevBookingArea$cepCalendar$selectReturn" :"2018-09-01",
+    "ctl00$FullRegion$MainRegion$ContentRegion$ContentFullRegion$ContentLeftRegion$CEPGroup1$CEPActive$cepNDPRevBookingArea$FlexDateSelector" :"Show selected dates",
+    "ctl00$FullRegion$MainRegion$ContentRegion$ContentFullRegion$ContentLeftRegion$CEPGroup1$CEPActive$cepNDPRevBookingArea$cepPassengerTypes$passengerTypeAdult" :"1",
+    "ctl00$FullRegion$MainRegion$ContentRegion$ContentFullRegion$ContentLeftRegion$CEPGroup1$CEPActive$cepNDPRevBookingArea$cepPassengerTypes$passengerTypeChild211" :"0",
+    "ctl00$FullRegion$MainRegion$ContentRegion$ContentFullRegion$ContentLeftRegion$CEPGroup1$CEPActive$cepNDPRevBookingArea$cepPassengerTypes$passengerTypeInfant" :"0",
+    "ctl00$FullRegion$MainRegion$ContentRegion$ContentFullRegion$ContentLeftRegion$CEPGroup1$CEPActive$cepNDPRevBookingArea$hdnsetDefaultValue" :"true",
+    "ctl00$FullRegion$MainRegion$ContentRegion$ContentFullRegion$ContentLeftRegion$CEPGroup1$CEPActive$cepNDPRevBookingArea$hdncalendarDropdown" :"true",
+    #"__PREVIOUSPAGE" :"3aoIK5urOF6qLmjEUVWoe7Zlok_H7Ef8UkS2oCFR_Ccg24aQSIRhidbF3PGeuRmIFTuiGxx8ealPNKfgqBWh77mCC2k1",
+}
+
+button_name = "ctl00$FullRegion$MainRegion$ContentRegion$ContentFullRegion$ContentLeftRegion$CEPGroup1$CEPActive$cepNDPRevBookingArea$Searchbtn$ButtonLink"
+
+def requests_download_html(url, payload):
+    import requests
+    with requests.post(url, payload, allow_redirects = True) as resp:
+        print(resp.content)
+
+def selenium_download_html(url, payload, submit_button):
+    from selenium import webdriver
+    from selenium.common.exceptions import ElementNotVisibleException
+
+    opts = webdriver.ChromeOptions()
+    opts.binary_location = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\Chrome.exe"
+    browser = webdriver.Chrome(options = opts)
+    browser.get(url)
+    #clear log file
+    with open("stderr.log", "w", encoding = "utf-8") as err:
+        
+        for key, value in payload.items():
+            elem = browser.find_element_by_name(key)
+            if elem.is_displayed:
+                try:
+                    elem.send_keys(value)
+                except ElementNotVisibleException as e:
+                    err.write("{}\n{}\n".format(elem, e))
+                    continue
+        #class_names = ["buttonContainer", "bluebutton", "buttonBody"]
+        #ids = ["ctl00"]
+        search_button_xpath = "//*[@id=\"ctl00_FullRegion_MainRegion_ContentRegion_ContentFullRegion_ContentLeftRegion_CEPGroup1_CEPActive_cepNDPRevBookingArea_Searchbtn\"]"
+        search = browser.find_element_by_xpath(search_button_xpath)
+        try:
+            search.click()
+        except ElementNotVisibleException as e:
+            err.write(e)
+
+        #for class_name in class_names:
+        #    search = browser.find_element_by_class_name(class_name)
+        #    try:
+        #        search.click()
+        #    except ElementNotVisibleException as e:
+        #        print("fuck")
+        #        err.write("")
+    
+    x = input("enter any key to end...")
+    print(x)
+    #print(browser.page_source)
+
+
+#<a id="ctl00_FullRegion_MainRegion_ContentRegion_ContentFullRegion_ContentLeftRegion_CEPGroup1_CEPActive_cepNDPRevBookingArea_Searchbtn_ButtonLink" href='javascript:WebForm_DoPostBackWithOptions(new WebForm_PostBackOptions("ctl00$FullRegion$MainRegion$ContentRegion$ContentFullRegion$ContentLeftRegion$CEPGroup1$CEPActive$cepNDPRevBookingArea$Searchbtn$ButtonLink", "", true, "", "", false, true))'><span class="buttonLeft"></span><span class="buttonBody">Search</span><span class="buttonRight"></span></a>
+if __name__ == "__main__":
+    selenium_download_html(flysas_url, payload, "bluebutton")
