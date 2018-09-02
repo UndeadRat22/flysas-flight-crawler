140b4b7553122e442fe0412914a8b8f35960017c
Valdas Rakutis <36191877+UndeadRat22@users.noreply.github.com>
2018-09-02 11:40:10 +0300

    Add files via upload
    gets pretty much all of the data now, just need to grab taxez.

--- flysas_crawler.py
+++ flysas_crawler.py
@@ -16,31 +16,69 @@ def scrape_data(filename):
     departure_table = soup.find("table", class_ = "WDSEffect_table", id = "WDSEffect_table_0").find("tbody").find_all("tr")
 
     all_prices = {}
+    __id = None
     for departure_container in departure_table:
-        if "id" not in departure_container.attrs:
+        #check if there's a class name, if not, we skippedy skip
+        if "class" not in departure_container.attrs:
             continue
-        __id = departure_container.attrs["id"]
-        prices_wrapper = departure_container.find_all("td", class_ = "fare")
-        flight_prices = []
-        for wrapped_price in prices_wrapper:
-            price = wrapped_price.find("div", class_ = "choice")
-            price = price.find("span", class_ = "price")
-            price = price.find("span", class_ = "number")
+        c = departure_container.attrs["class"]
+        #ignore the empty containers
+        if "empty" in c:
+            continue
+        #set the __id variable if we're working on some obj
+        if "id" in departure_container.attrs:
+            __id = departure_container.attrs["id"]
+        #parse inner html
+        #print(c)
+        if ((c == ["flight", "EffectOff", "EffectOff_nofamily", "segmented"])
+        or (c == ['flight', 'segmented', 'SelectedEffectOff', 'SelectedEffectOff_family1'])):
+            print("flight prices ", end = "")
+            prices = get_flightdata(departure_container)
+            print(prices)
+            continue
+        
+        #TODO: ?
+        if ((c == ["flight", "segments", "EffectOff", "EffectOff_nofamily"])
+        or (c == ['flight', 'segments', 'SelectedEffectOff', 'SelectedEffectOff_family1'])):
+            get_stop(departure_container)
+            continue
 
-            price_string = price.attrs["data-price"]
-            flight_prices.append(price_string)
-        if len(flight_prices) > 0:
-            all_prices[__id] = flight_prices
+def get_stop(container):
+    stop_wrapper = container.find("td").find("div").find("table").find("tbody")
+    flightinfos = stop_wrapper.find_all("tr", class_ = "flightInfo")
+    for info in flightinfos:
+        stop = info.find("td", class_ = "stopover")
+        if not stop:
+            continue
+        stop = stop.find("div").find("span", class_ = "label")
+        if not stop:
+            continue
+        print(stop.text)
 
-    for f in all_prices.items():
-        print(f)
-        #print(departure_container.text)
+def get_flightdata(container):
+    prices_wrapper = container.find_all("td", class_ = "fare")
+    flight_prices = []
+    for wrapped_price in prices_wrapper:
+        price = wrapped_price.find("div", class_ = "choice")
+        price = price.find("span", class_ = "price")
+        price = price.find("span", class_ = "number")
 
-    #dep_table_soup = BeautifulSoup(departure_table.html(), "html.parser")
-    #trs = dep_table_soup.find_all("tr")
-    #for tr in trs:
-    #    print(tr.text())
+        if "data-price" not in price.attrs:
+            continue
+        price_string = price.attrs["data-price"]
+        flight_prices.append(price_string)
+    time_wrapper = container.find("td", class_ = "time")
+    times = time_wrapper.find_all("span", class_ = "time")
+    starttime = times[0].text.replace(" ", "")
+    endtime = times[1].text.replace(" ", "")
 
+    airport_wrapper = container.find("td", class_ = "airport")
+    airports = airport_wrapper.find_all("acronym", class_ = "airport")
+    #haven't seen any spaces but just incase
+    startport = airports[0].text.replace(" ", "")
+    endport = airports[1].text.replace(" ", "")
+
+    return [flight_prices, starttime, endtime, startport, endport]
 
 if __name__ == "__main__":
     scrape_data("D:\\temp\\index.html")
