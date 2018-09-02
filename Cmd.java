9a6a5423f9200be48854370b9b8467c51bc3e455
Valdas Rakutis <36191877+UndeadRat22@users.noreply.github.com>
2018-09-02 22:38:50 +0300

    Update Cmd.java
    added pythonCodePath as a variable

--- Cmd.java
+++ Cmd.java
@@ -4,12 +4,16 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Cmd {
+    private String pythonExecCmd;
     public Cmd(){
-
+        this("D:\\_root\\programming\\Python\\infare\\flysas\\");
+    }
+    public Cmd(String pythonCodePath){
+        this.pythonExecCmd = "cd \"" + pythonCodePath + "\" && python .\\flysas_crawler.py";
     }
     public void run() throws Exception {
         ProcessBuilder builder = new ProcessBuilder(
-                "cmd.exe", "/c", "cd \"D:\\_root\\programming\\Python\\infare\\flysas\\\" && python .\\flysas_crawler.py");
+                "cmd.exe", "/c", pythonExecCmd);
         builder.redirectErrorStream(true);
         Process p = builder.start();
         BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
